a48380161ae27a3ce787dccdb031340c2eb5ab9a
krzysztof-pankow <kpankow@dinga.ai>
2024-09-22 16:13:05 +0200

    add tests for model node

---
+++ tests/test_model_node.py
@@ -0,0 +1,67 @@
+from nodemodel.model_node import ModelNode
+import networkx as nx
+
+def test_model_node_with_forced_values():
+    def a(x,y):
+        pass
+    node_name = "a"
+    nodes = {"a":a}
+    nodes_with_forced_nodes = {"a":{"x":1,"y":2}}
+    graph = nx.DiGraph()
+    graph.add_edges_from([(("x",1),"a"),(("y",2),"a")])
+
+    model_node = ModelNode(node_name,nodes,nodes_with_forced_nodes,graph)
+    assert model_node.compute == a
+    assert model_node.inputs == [("x",1),("y",2)]
+
+def test_model_node_with_forced_values_no_arguments():
+    def a():
+        pass
+    node_name = "a"
+    nodes = {"a":a}
+    nodes_with_forced_nodes = {"a":{"x":1,"y":2}}
+    graph = nx.DiGraph()
+    graph.add_nodes_from(["a"])
+    model_node = ModelNode(node_name,nodes,nodes_with_forced_nodes,graph)
+
+    assert model_node.compute == a
+    assert model_node.inputs == []
+
+def test_model_node_simple():
+    def a(x,y):
+        pass
+    node_name = "a"
+    nodes = {"a":a}
+    nodes_with_forced_nodes = {}
+    graph = nx.DiGraph()
+    graph.add_edges_from([("x","a"),("y","a")])
+    model_node = ModelNode(node_name,nodes,nodes_with_forced_nodes,graph)
+
+    assert model_node.compute == a
+    assert model_node.inputs == ["x","y"]
+
+
+def test_model_node_forced_value():
+    node_name = ("x",2)
+    nodes = {}
+    nodes_with_forced_nodes = {}
+    graph = nx.DiGraph()
+    model_node = ModelNode(node_name,nodes,nodes_with_forced_nodes,graph)
+
+    assert model_node.compute() == 2
+    assert model_node.inputs == []
+
+def test_model_node_auxiliary():
+    def a(x,y):
+        pass
+    node_name = ("a","x",1,"y",2)
+    nodes = {"a":a}
+    nodes_with_forced_nodes = {"a":{"x":1,"y":2}}
+    graph = nx.DiGraph()
+    graph.add_edges_from([(("x",1),("a","x",1,"y",2)),(("y",2),("a","x",1,"y",2))])
+
+    model_node = ModelNode(node_name,nodes,nodes_with_forced_nodes,graph)
+    assert model_node.compute == a
+    assert model_node.inputs == [("x",1),("y",2)]
+
+
